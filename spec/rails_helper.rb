8a9a5c0793e220c6a6ad896b74c3271900b440ea
Justine <jjtroyke@gmail.com>
2018-05-12 23:20:23 -0600

    add article spec with validations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -55,10 +56,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    # Choose a test framework:
-    with.test_framework :rspec
-    # Or, choose the following (which implies all of the above):
-    with.library :rails
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+      # Or, choose the following (which implies all of the above):
+      with.library :rails
+    end
   end
 end
